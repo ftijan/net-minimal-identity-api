98d17d70175b1fcd807dba0230a904979a1889ad
ftijan <41797259+ftijan@users.noreply.github.com>
2023-10-14 19:31:22 +0100

    add solution files and docs (#1)
    * update gitignore
    * add code
    * update docs

---
+++ src/Example.MinimalIdentityApi/AppIdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.MinimalIdentityApi
+{
+	public class AppIdentityContext : IdentityDbContext<AppIdentityUser>
+	{
+        public AppIdentityContext(DbContextOptions<AppIdentityContext> options) : base(options)
+        {
+            
+        }
+    }
+}
